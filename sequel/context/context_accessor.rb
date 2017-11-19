2f8143ccb88936c1d2b6e3947e24a7ce677fa39e
Taiga Tsutsumi <nowavailable@users.noreply.github.com>
2017-11-19 23:04:57 +0900

    コメントの追加。

--- sequel/context/context_accessor.rb
+++ sequel/context/context_accessor.rb
@@ -5,10 +5,19 @@ module ContextAccessor
   def context=(ctx)
     Thread.current[:context] = ctx
   end
+  # コンテキスト内でこのブロックを使用すると、そのブロック内で
+  # 利用される（ロール等の）オブジェクト内から、
+  # コンテキストのアクセサメソッドにアクセス可能になる。
+  # （コンテキストのアクセサメソッドをスレッドローカルな変数として共有するから）
+  # 注入された部品であるロールが、注入される母体であるコンテキストの
+  # 構造を知っている状態にできるということ。つまりはIoCパターンの実現。
   def execute_in_context
+    # 退避処理
     origin_context_if_exist = self.context
+    # 具体的なContexインスタンスを、Thread.current[:context] に代入。
     self.context = self
     yield
+    # 退避したオブジェクトを書き戻し。
     self.context = origin_context_if_exist
   end
 end
--- sequel/role/delivery_proposer.rb
+++ sequel/role/delivery_proposer.rb
@@ -24,9 +24,11 @@ module DeliveryProposer
   # 注文に含まれる明細の一部だけを受注できる店舗のリストを
   # 商品ごとに、手数料率の高い順に並べて返す
   def shops_partial_profitable
+    # 注文に含まれる明細をすべて受注できる店舗があるなら、
+    # それらの店舗はは、このメソッドのの処理結果リストから
+    # 除く必要があるかも知れない。
     if context().shops_fullfilled_profitable() != [] or
         context().shops_fullfilled_leveled() != []
-      #
     end
     shops = []
     return shops
@@ -34,9 +36,11 @@ module DeliveryProposer
   # 注文に含まれる明細の一部だけを受注できる店舗のリストを
   # 商品ごとに、稼働の小さい順に並べて返す
   def shops_partial_leveled
+    # 注文に含まれる明細をすべて受注できる店舗があるなら、
+    # それらの店舗はは、このメソッドのの処理結果リストから
+    # 除く必要があるかも知れない。
     if context().shops_fullfilled_profitable() != [] or
         context().shops_fullfilled_leveled() != []
-      #
     end
     shops = []
     return shops
