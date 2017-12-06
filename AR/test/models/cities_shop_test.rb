23390e0c21741946df22cf375102a8c96a2da776
Taiga Tsutsumi <nowavailable@users.noreply.github.com>
2017-12-07 00:04:00 +0900

    不要ファイル削除

--- AR/test/models/cities_shop_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CitiesShopTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- AR/test/models/rule_for_ship_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RuleForShipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
